fcd370bdbb114f1ce067887b7c9e78d4d811fd9a
Callie Gorman <calliegorman4@gmail.com>
2018-06-29 21:39:50 -0400

    Second save

---
+++ app.js
@@ -0,0 +1,9 @@
+var modal = document.getElementbyClassName('box');
+
+var btn = document.getElementByClassName("btnLink");
+
+var close = document.getElementbyID("fa")[0];
+
+btn.onclick = function() {
+  modal.style.display= "block";
+}
