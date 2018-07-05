ee71245e9db22dab68ad76fd0ca304ffcf8f386b
Callie Gorman <calliegorman4@gmail.com>
2018-07-05 13:19:09 -0400

    Javascript for modal update; mobile responsive css upaupdate

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-<script>
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("btnLink");
+let buttons = document.querySelectorAll('.btnLink');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById('myModal');
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
+let span = document.getElementsByClassName("close")[0];
+
+buttons.forEach(function(button) {
+  button.onclick = function() {
     modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
+  }
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+});
